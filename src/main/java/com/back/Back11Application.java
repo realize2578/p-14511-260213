5e7dd1dbaf332ed4c4897be232cd35c5b8f74aad
realize2578 <realize2578@gmail.com>
2026-02-13 14:11:30 +0900

    [feat] : 프로젝트 생성

---
+++ src/main/java/com/back/Back11Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Back11Application.class, args);
+	}
+
+}
