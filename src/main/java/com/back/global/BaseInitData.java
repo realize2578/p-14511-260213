c748cc864e3d1ddcd9d6acbdae09c6ee2d86f227
realize2578 <realize2578@gmail.com>
2026-02-23 14:43:24 +0900

    [feat] : Transaction 추가

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -2,13 +2,22 @@ package com.back.global;
 
 import com.back.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BaseInitData {
+
+
+    @Autowired
+    @Lazy
+    private BaseInitData self;
 
 
     private final PostService postService;
@@ -16,27 +25,21 @@ public class BaseInitData {
     @Bean
     ApplicationRunner initDataRunner(){
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
+    @Transactional
     void work1(){
         if(postService.count() > 0) {
             return;
         }
 
-        try{
-
             postService.write("제목1", "내용1");
-            if(true){
-                throw new RuntimeException("테스트 예외");
-            }
 
             postService.write("제목2", "내용2");
-        }catch (Exception e){
-            System.out.println("예외 발생 :"+e.toString());
-        }
     }
+
     void work2(){
 
         postService.findById(1);
