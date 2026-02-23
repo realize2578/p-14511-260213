e8b72379b52b47a95812090f1271722750e204b7
realize2578 <realize2578@gmail.com>
2026-02-23 12:10:52 +0900

    [feat] : @RequiredArgsConstructor 어노테이션 도입

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -1,16 +1,17 @@
 package com.back.global;
 
 import com.back.domain.post.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class BaseInitData {
 
-    @Autowired
-    private PostService postService;
+
+    private final PostService postService;
 
     @Bean
     ApplicationRunner initDataRunner(){
